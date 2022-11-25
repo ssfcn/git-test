a8a77d493cda457e70b7f5c01ceb5f57186d8f59
songshuaifei <songshuaifei@sinosift.com.cn>
2022-11-25 22:41:06 +0800

    third commit

--- src/main/java/com/atgugui/git/Github.java
+++ src/main/java/com/atgugui/git/Github.java
@@ -4,6 +4,6 @@ public class Github {
     public static void main(String[] args) {
         System.out.println("github");
         System.out.println("github 2");
-        System.out.println("github");
+        System.out.println("github 3");
     }
 }
