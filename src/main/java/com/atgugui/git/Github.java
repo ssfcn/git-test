b5bb1b3e913754d85ade6b518ac641027874e23b
songshuaifei <songshuaifei@sinosift.com.cn>
2022-11-25 22:54:54 +0800

    master commit-2

--- src/main/java/com/atgugui/git/Github.java
+++ src/main/java/com/atgugui/git/Github.java
@@ -6,5 +6,6 @@ public class Github {
         System.out.println("github 2");
         System.out.println("github 3");
         System.out.println("github 4");
+        System.out.println("master test");
     }
 }
