4e83f0497a4959c0ebd395e4cdd9e01f32399be5
songshuaifei <songshuaifei@sinosift.com.cn>
2022-11-25 23:40:16 +0800

    master stash-test04

--- src/main/java/com/atgugui/git/Github.java
+++ src/main/java/com/atgugui/git/Github.java
@@ -9,5 +9,6 @@ public class Github {
         System.out.println("hot-fix test2");
         System.out.println("master test2");
         System.out.println("master stash-test03");
+        System.out.println("master stash-test04");
     }
 }
